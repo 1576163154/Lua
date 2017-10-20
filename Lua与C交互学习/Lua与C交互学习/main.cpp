f143eb76044d8aa2d203e5abe7b0b836ff60e61a
NT <NT@XXJIANG-PC>
2017-10-20 11:54:51 +0800

    lua_pushlstring和lua_pushstring

--- Lua与C交互学习/Lua与C交互学习/main.cpp
+++ Lua与C交互学习/Lua与C交互学习/main.cpp
@@ -4,8 +4,17 @@
 using namespace std;
 
 void main() {
+	lua_State* l = luaL_newstate();
+	
+	//lua_pushlstring 使用
+	lua_pushlstring(l, "abcd",5);
+	size_t* length = nullptr;
+	
+	const char* a = lua_tolstring(l, -1,length);
 
 
-
+	//lua_pushstring 使用
+	lua_pushstring(l, "bcdef");
+	const char* b = lua_tostring(l, -1);
 	system("pause");
 }
