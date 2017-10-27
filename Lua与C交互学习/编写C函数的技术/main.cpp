040a3087fcb54761928596fa8cd2ca27c97a43d6
Xiang <849483053@qq.com>
2017-10-27 18:27:25 +0800

    调用C函数处理table

--- Lua与C交互学习/编写C函数的技术/main.cpp
+++ Lua与C交互学习/编写C函数的技术/main.cpp
@@ -17,17 +17,44 @@ int l_map(lua_State* l) {
 		lua_call(l, 1, 1);
 		lua_rawseti(l, 1, i);
 	}
+
+	return 0; //无结果
+}
+
+static int l_split(lua_State* l) {
+	const char* s = luaL_checkstring(l, 1);
+	const char* sep = luaL_checkstring(l, 2);
+	const char* e;
+	int i = 1;
+
+
+	lua_newtable(l);//创建一个table放到栈顶
+
+	while((e == strchr(s, *sep)) != NULL) {
+		lua_pushlstring(l, s, e - s);
+		lua_rawseti(l, -2, i++);
+		s = e + 1;
+	}
+
+	lua_pushstring(l, s);
+	lua_rawseti(l, -2, i);
+
+	return 1;
+
+
 }
 
 void main() {
 	lua_State* l =  luaL_newstate();
 	luaL_openlibs(l);
 
-	lua_newtable(l);
+	//load lua file
+	luaL_loadfile(l, "D:\\GitHub\\Lua\\Lua与C交互学习\\编写C函数的技术\\test.lua");
 
+	lua_pushcfunction(l, l_map);
+	lua_setglobal(l, "mapSquare");
 
-
-
+	luaL_dofile(l, "D:\\GitHub\\Lua\\Lua与C交互学习\\编写C函数的技术\\test.lua");
 
 	system("pause");
 }
---
+++ Lua与C交互学习/编写C函数的技术/test.lua
@@ -0,0 +1,13 @@
+
+t = {1,2,3,4}
+
+function square(n)
+return n*n
+end
+
+
+mapSquare(t,square)
+
+for i =1,#t do
+    print(t[i])
+end
